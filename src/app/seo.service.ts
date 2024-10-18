08c3c43c225738126d55e70288b71db6033ce67e
christian-supy-io <christian@supy.io>
2024-10-19 00:06:05 +0300

    quick fixes

--- src/app/seo.service.ts
+++ src/app/seo.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SeoService {
-  constructor(private meta: Meta, private title: Title) {}
+  constructor(
+    private meta: Meta,
+    private title: Title,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
 
   updateTitle(title: string) {
     this.title.setTitle(title);
@@ -20,10 +25,15 @@ export class SeoService {
   }
 
   updateOgTags(title: string, description: string, image: string) {
-    this.meta.updateTag({ property: 'og:title', content: title });
-    this.meta.updateTag({ property: 'og:description', content: description });
-    this.meta.updateTag({ property: 'og:image', content: image });
-    // this.meta.updateTag({ property: 'og:url', content: window.location.href });
-    this.meta.updateTag({ property: 'og:type', content: 'website' });
+    if (isPlatformBrowser(this.platformId)) {
+      this.meta.updateTag({ property: 'og:title', content: title });
+      this.meta.updateTag({ property: 'og:description', content: description });
+      this.meta.updateTag({ property: 'og:image', content: image });
+      this.meta.updateTag({
+        property: 'og:url',
+        content: window.location.href,
+      });
+      this.meta.updateTag({ property: 'og:type', content: 'website' });
+    }
   }
 }
